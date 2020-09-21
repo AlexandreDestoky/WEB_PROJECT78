ceb4a293fc1f578011e3ffb1322deee202a55755
Alexandre Destoky <adestoky@gmail.com>
2020-09-21 15:33:22 +0200

    Liste détails des posts commit

--- app/vues/template/index.php
+++ app/vues/template/index.php
@@ -4,16 +4,10 @@
     <?php include '../app/vues/template/partials/_head.php'; ?>
 </head>
 <body>
-
-<?php include '../app/vues/template/partials/_preload.php'; ?>
-
-<?php include '../app/vues/template/partials/_header.php'; ?>
-
-<?php include '../app/vues/template/partials/_main.php'; ?>
-
-<?php include '../app/vues/template/partials/_footer.php'; ?>
-
-<?php include '../app/vues/template/partials/_scripts.php'; ?>
-
+  <?php include '../app/vues/template/partials/_preload.php'; ?>
+  <?php include '../app/vues/template/partials/_header.php'; ?>
+  <?php include '../app/vues/template/partials/_main.php'; ?>
+  <?php include '../app/vues/template/partials/_footer.php'; ?>
+  <?php include '../app/vues/template/partials/_scripts.php'; ?>
 </body>
 </html>
--- noyau/init.php
+++ noyau/init.php
@@ -1,7 +1,10 @@
+
 <?php
 /*
   ./noyau/init.php
  */
 
   require_once '../app/config/params.php';
+  require_once '../noyau/constantes.php';
   require_once '../noyau/connexion.php';
+  require_once '../noyau/fonctions.php';
